6cae1fa85a819c2a15629050fa40caa1ee2ff067
Gavindu Sandeepa Samarawickrama <gavindusandeepa555@gmail.com>
2026-06-27 15:04:48 +0530

    Comment out all JavaScript code in app.js for cleanup

--- js/app.js
+++ js/app.js
@@ -1,1 +1,85 @@
-console.log("Hello Java Script ::)");
+// let name = "Saman";
+
+// let age = 18;
+
+// name = "ranil";
+
+// console.log(name);
+// console.log(age);
+// alert("Hello HTML");
+
+
+// let name = "Saman";
+// let age = 18;
+// let isAfterAL = false;
+// let customers = [];
+// let customer = {};
+// let salary = 75000.0;
+
+// console.log(name);
+// console.log(age);
+// console.log(isAfterAL);
+// console.log(customers);
+// console.log(customer);
+// console.log(salary);
+
+
+// let x = 10;
+// let y = 20;
+
+// let z = x * y;
+
+// console.log(z);
+
+
+// let x = 10;
+
+// let y = "10";
+
+// console.log(x===y);
+
+// -----------------------------------------------------------
+
+//  logical operators
+
+//  && | || | !
+
+// if (!false) {
+//     console.log("true");
+// } else {
+//     console.log("false");
+// }
+
+
+// let x = 10;
+// let y = 20;
+
+// if (x > y) {
+//     console.log("x is greater than y");
+// } else if (x < y) {
+//     console.log("x is less than y");
+// } else {
+//     console.log("x is equal to y");
+// }
+
+
+// let day = 2;
+
+// switch (day) {
+//     case 1:
+//         console.log("Monday");
+//     case 2:
+//         console.log("Tuesday");
+//     case 3:
+//         console.log("Wednesday");
+//         break;
+//     default:
+//         console.log("Invalid day");
+// }
+
+
+// for (let i = 0; i < 10; i++) {
+//     console.log(i);
+// }
+
+
